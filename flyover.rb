2ba76af5614bc41b528bd4fb1545635fb822463e
Theresa Fister <tfister97@gmail.com>
2016-06-26 02:45:29 +0000

    Done.

--- flyover.rb
+++ flyover.rb
@@ -1,1 +1,43 @@
 ## Code your solution here.
+def fasten_seatbelt
+  return "Alright foaks. Make sure to fasten your seatbelt!"
+end
+
+def free_to_move_about_the_cabin
+  return "Now it's safe to move about the cabin, if you so desire."
+end
+
+def pennsylvania
+  return "We're flying over pimptastic Pennsylvania!"
+end
+
+def ohio
+  return "We are over the one, the only.... Ohio!".upcase
+end
+
+def indiana
+  return "Now we're at irreplaceable Indiana!".upcase
+end
+
+def illinois
+  return "Look out now to see illusional Illinois!".reverse
+end
+
+def iowa
+  return "We're at irresitable Iowa, now!"
+end
+
+def nebraska
+  return "Wow. Can't belive we're at never-ending Nebraska."
+end
+
+def colorado
+  return "Hold on to your hats! We're at CRAAAAAZYYYY colorado!".upcase
+end
+
+def utah
+  return "Almost there, folks. Look out now for understanding Utah.".reverse
+end
+
+def cali
+  return "WE DID IT! Welcome to cathartic California!"
